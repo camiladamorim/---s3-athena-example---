cd7749c60ea7b7573c8472ca58fdbf2f1b70b1e1
camiladamorim <camilabcdamorim@gmail.com>
2020-07-17 21:59:58 -0300

    ..

--- ninacode/run.py
+++ ninacode/run.py
@@ -1,33 +1,14 @@
 from decouple import config
-from pipelines.jurimetria import JurimetriaPipeline
-
+from ninacode.variaveis import Pipeline
 
 def main():
 
-    # Pipeline parameters
-    input_filepath = config('INPUT_FILEPATH')
-    output_filepath = config('OUTPUT_FILEPATH')
+#parameters
+    path_whitelist = config('PATH_DATA', default='data/student.csv')
 
-    # Steps parameters
-    path_whitelist = config(
-        'PATH_WHITELIST',
-        default='pipelines/jurimetria/data/whitelist.csv'
-    )
-    path_lista_advogados = config(
-        'PATH_LISTA_ADVOGADOS',
-        default='pipelines/jurimetria/data/lista_advogados.csv'
-    )
-    path_classe_cnj_cliente = config(
-        'PATH_CLASSE_CNJ_CLIENTE',
-        default='pipelines/jurimetria/data/classe_cnj_cliente.csv'
-    )
+    with Pipeline(path_whitelist) as pipeline:
+        pipeline.run()
 
-    with JurimetriaPipeline(input_filepath, output_filepath) as pipeline:
-        pipeline.run(
-            path_whitelist=path_whitelist,
-            path_lista_advogados=path_lista_advogados,
-            path_classe_cnj_cliente=path_classe_cnj_cliente
-        )
 
 
 if __name__ == '__main__':
